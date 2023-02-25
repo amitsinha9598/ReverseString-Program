1023edd6d9b654252c9b990bcd071cdee75d0596
amitsinha9598 <103451609+amitsinha9598@users.noreply.github.com>
2023-02-25 15:14:50 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+import java .lang.*;
+import java util.*;
+class ReverseString
+{
+public static void main(String[] args)
+{
+String input =ABCD;
+byte[] strAsByteArray= input.getBytes();
+byte[] result=new byte[strAsByteArray.length];
+for (int i=0;i<strByteArray.length; i++)
+result [i]=atrAsByteArray[strAsByteArray.length -i-1];
+System.out.println(new String(result));
+}
+
